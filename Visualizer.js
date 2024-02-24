96c2854aca110bb7ca9d9206c3db2e8f4bb8bf8e
epi0001 <68769035+epi0001@users.noreply.github.com>
2024-02-23 17:53:12 -0800

    Sort v1.2
    Added delay while sorting so you can see it happening, made the width dependent on amount of bars, added a counter for the bars.

--- Visualizer.js
+++ Visualizer.js
@@ -1,11 +1,15 @@
 var list = []
 var amount;
+var totalWidth = 40;
+var delay = 50;
 
 // submit the chosen amount of items to sort
 function submit() {
     reset();
     amountInput = document.getElementById("amount");
     amount = amountInput.value;
+    amountDisplay = document.getElementById("amountDisplay")
+    amountDisplay.textContent = "Amount: " + String(amount)
     list.push(1,2);
     for (let num = 3; num <= amount; num++) {
         list.push(num);
@@ -25,17 +29,12 @@ function shuffle(array) {
     return array;
 }
 
-//sleep function
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 //Creating the divs for sorting visualization
 function setup() {
     list = shuffle(list)
     for (let i = 0; i < amount; i++) {
         var div = document.createElement("div");
-        div.style.width = "5%";
+        div.style.width = String(totalWidth / amount) + "vw";
         div.style.background = "green";
         div.style.margin = "5px";
         div.style.height = String(list[i]*30) + "px";
@@ -57,18 +56,28 @@ function reset() {
 
 // calling the selected sorting algo
 function sort() {
+    curr_i = 0;
+    curr_j = 0;
     inputtedAlgo = document.getElementById("algos");
     algo = inputtedAlgo.value;
     if (algo == "bubbleSort") {
-        bubbleSort(list);
+        list = bubbleSort(list)
     }
 }
+async function visualizeSort(list, j) {
+    // matching the height of the divs with the new values
+    document.getElementById('div'+String(j+1)).style.height = String(list[j]*30) + "px";
+    document.getElementById('div'+String(j+1)).style.marginTop = String(600 - list[j]*30) + "px";
+    document.getElementById('div'+String(j+2)).style.height = String(list[j+1]*30) + "px";
+    document.getElementById('div'+String(j+2)).style.marginTop = String(600 - list[j+1]*30) + "px";
 
+    // Simulate a delay to visualize the sorting process
+    await new Promise(resolve => setTimeout(resolve, delay));
+}
 
 // sorting algorithms js
-function bubbleSort(list) { 
+async function bubbleSort(list) { 
     for (var i = 0; i < list.length; i++) {
-
         for (var j = 0; j < (list.length - i - 1); j++) { 
 
             //checking if the two element need to be swapped
@@ -79,15 +88,11 @@ function bubbleSort(list) {
                 list[j] = list[j + 1]
                 list[j + 1] = temp
 
-                // matching the height of the divs with the new values
-                document.getElementById('div'+String(j+1)).style.height = String(list[j]*30) + "px";
-                document.getElementById('div'+String(j+1)).style.marginTop = String(600 - list[j]*30) + "px";
-                document.getElementById('div'+String(j+2)).style.height = String(list[j+1]*30) + "px";
-                document.getElementById('div'+String(j+2)).style.marginTop = String(600 - list[j+1]*30) + "px";
+                await visualizeSort(list, j);
             }
         } 
     }
-    return list
+    return list;
 }
  
 
