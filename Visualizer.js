f67829c742193bf02ba16f2f4033d73396b786f5
mmythen <159862998+mmythen@users.noreply.github.com>
2024-02-24 15:31:05 -0800

    Add files via upload

--- Visualizer.js
+++ Visualizer.js
@@ -1,96 +1,154 @@
 var list = []
 var amount;
+var totalHeight = 40; // in vw
+var totalWidth = 40; // in vw
+var delay = 35; // in ms
+var barColour = "green"
 
-// submit the chosen amount of items to sort
 function submit() {
     reset();
     amountInput = document.getElementById("amount");
     amount = amountInput.value;
-    list.push(1,2);
+    amountDisplay = document.getElementById("amountDisplay")
+    amountDisplay.textContent = "Amount: " + String(amount)
+    list.push(1, 2);
     for (let num = 3; num <= amount; num++) {
         list.push(num);
     }
     setup();
 }
 
-//function to randomly shuffle MY list for sorting
 function shuffle(array) {
     let currentIndex = array.length;
     while (currentIndex > 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]
+        ];
     }
     return array;
 }
 
-//sleep function
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-//Creating the divs for sorting visualization
 function setup() {
-    list = shuffle(list)
+    list = shuffle(list);
     for (let i = 0; i < amount; i++) {
-        var div = document.createElement("div");
-        div.style.width = "5%";
+        let div = document.createElement("div");
+        div.style.width = String(totalWidth / amount) + "vw";
         div.style.background = "green";
         div.style.margin = "5px";
-        div.style.height = String(list[i]*30) + "px";
-        div.style.marginTop = String(600 - list[i]*30) + "px";
-        div.id = "div"+(i+1);
+        div.style.height = String(list[i] * (600/list.length)) + "px";
+        div.style.marginTop = String(600 - list[i] * (600/list.length)) + "px";
+        div.id = "div" + (i + 1);
         document.getElementById("main").appendChild(div);
     }
 }
 
-// resetting the display
 function reset() {
     list = [];
-    for (let i = 0; i < amount; i++) {
-        var div = document.createElement("div");
-        div.id = "div"+(i+1);
-        document.getElementById("div"+(i+1)).remove();
+    let main = document.getElementById("main");
+    while (main.firstChild) {
+        main.removeChild(main.firstChild);
     }
 }
 
-// calling the selected sorting algo
+function updateSlider() {
+    amountInput = document.getElementById("amount");
+    tempAmount = amountInput.value;
+    amountDisplay = document.getElementById("amountDisplay");
+    amountDisplay.textContent = "Amount: " + String(tempAmount);
+}
+
+function setTheme(themeName) {
+    document.documentElement.className = themeName;
+    switch (themeName) {
+        case 'theme-default':
+            barColour = 'green';
+            break;
+        
+        case 'theme-pink':
+            barColour = 'rgb(250, 100, 255)';
+            break;
+    
+        case 'theme-bw':
+            barColour = '#fff';
+            break;
+
+        default:
+            barColour = 'green';
+    }
+    for (let i = 0; i < list.length; i++) {
+        visualizeSort(list, i);
+    }
+}
+
 function sort() {
+    curr_i = 0;
+    curr_j = 0;
     inputtedAlgo = document.getElementById("algos");
-    algo = inputtedAlgo.value;
-    if (algo == "bubbleSort") {
-        bubbleSort(list);
+
+    switch (inputtedAlgo.value) {
+        case "bubbleSort":
+            bubbleSort(list);
+            break;
+        case "bozoSort":
+            bozoSort(list);
+            break;
     }
 }
 
+async function visualizeSort(list, indices) {
+    for (let j of indices) {
+        document.getElementById('div' + String(j + 1)).style.height = String(list[j] * (600/list.length)) + "px";
+        document.getElementById('div' + String(j + 1)).style.marginTop = String(600 - list[j] * (600/list.length)) + "px";
+        await new Promise(resolve => setTimeout(resolve, delay));
+    }
+}
 
-// sorting algorithms js
-function bubbleSort(list) { 
-    for (var i = 0; i < list.length; i++) {
+async function bubbleSort(list) {
+    for (let i = 0; i < list.length; i++) {
+        for (let j = 0; j < (list.length - i - 1); j++) {
 
-        for (var j = 0; j < (list.length - i - 1); j++) { 
+            if (list[j] > list[j + 1]) {
+                let temp = list[j];
+                list[j] = list[j + 1];
+                list[j + 1] = temp;
 
-            //checking if the two element need to be swapped
-            if (list[j] > list[j + 1]) { 
-
-                //swapping the 2 values in list
-                var temp = list[j] 
-                list[j] = list[j + 1]
-                list[j + 1] = temp
-
-                // matching the height of the divs with the new values
-                document.getElementById('div'+String(j+1)).style.height = String(list[j]*30) + "px";
-                document.getElementById('div'+String(j+1)).style.marginTop = String(600 - list[j]*30) + "px";
-                document.getElementById('div'+String(j+2)).style.height = String(list[j+1]*30) + "px";
-                document.getElementById('div'+String(j+2)).style.marginTop = String(600 - list[j+1]*30) + "px";
+                await visualizeSort(list, [j, j + 1]);
             }
-        } 
+        }
     }
-    return list
+    return list;
 }
- 
 
+async function bozoSort(list) {
+    let sorted = false;
+    while (!sorted) {
+        let index1 = Math.floor(Math.random() * list.length);
+        let index2 = Math.floor(Math.random() * list.length);
+
+        // Swap the elements
+        let temp = list[index1];
+        list[index1] = list[index2];
+        list[index2] = temp;
+
+        // Visualize the step with a delay
+        await visualizeSort(list, [index1, index2]);
+
+        // Check if the list is sorted
+        sorted = isSorted(list);
+    }
+    return list;
+}
+
+function isSorted(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 
 
